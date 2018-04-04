3594935354e087f5c4a6113676ee4ae3c1a97c4c
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2018-04-04 00:42:01 -0700

    add components, eslint

---
+++ src/components/search-bar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={event => this.setState({ term: event.target.value })}
+          value={this.state.term} />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search-bar';
 // import './index.css';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
@@ -7,9 +8,15 @@ import ReactDOM from 'react-dom';
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
+const API_KEY = 'AIzaSyBGC8P3FA40zy94S217tbCR3uvXRCDFb_g';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
